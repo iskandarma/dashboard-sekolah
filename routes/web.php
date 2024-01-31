002fcfe59b7344e31bdffed2ff377782ac0507dc
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2024-02-01 00:09:14 +0700

    implement template web and dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,33 +17,33 @@ use App\Http\Controllers\KategoriController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('layouts/web/index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/home', function () {
+    return view('layouts/dashboard/index');
 });
 
-Route::get('/login', 'App\Http\Controllers\LoginController@index');
+// Route::get('/login', 'App\Http\Controllers\LoginController@index');
 
-Route::get('userlevels', 'App\Http\Controllers\UserlevelController@data');
+// Route::get('userlevels', 'App\Http\Controllers\UserlevelController@data');
 
-Route::get('userlevels/add', 'App\Http\Controllers\UserlevelController@add');
+// Route::get('userlevels/add', 'App\Http\Controllers\UserlevelController@add');
 
-Route::post('userlevels', 'App\Http\Controllers\UserlevelController@addProcess');
+// Route::post('userlevels', 'App\Http\Controllers\UserlevelController@addProcess');
 
-Route::get('userlevels/edit/{id}', 'App\Http\Controllers\UserlevelController@edit');
+// Route::get('userlevels/edit/{id}', 'App\Http\Controllers\UserlevelController@edit');
 
-Route::patch('userlevels/{id}', 'App\Http\Controllers\UserlevelController@editProcess');
+// Route::patch('userlevels/{id}', 'App\Http\Controllers\UserlevelController@editProcess');
 
-Route::delete('userlevels/{id}', 'App\Http\Controllers\UserlevelController@delete');
+// Route::delete('userlevels/{id}', 'App\Http\Controllers\UserlevelController@delete');
 
-Route::get('kategoris/add', 'App\Http\Controllers\UserlevelController@add');
+// Route::get('kategoris/add', 'App\Http\Controllers\UserlevelController@add');
 
-Route::resource('kategoris', KategoriController::class);
+// Route::resource('kategoris', KategoriController::class);
 
-Route::resource('barangs', BarangController::class);
+// Route::resource('barangs', BarangController::class);
 
-Route::resource('submits', SubmitController::class);
+// Route::resource('submits', SubmitController::class);
 
-Route::get('jumlahbarang/{id}', 'App\Http\Controllers\BarangController@getjumlahbarang');
+// Route::get('jumlahbarang/{id}', 'App\Http\Controllers\BarangController@getjumlahbarang');
