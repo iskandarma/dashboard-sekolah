009417e1faab496ec5ff39a6d367fee93eedec69
NalendraUtama <nalendrautama@gmail.com>
2024-02-06 14:17:02 +0700

    memperbaiki fitur sesuai role part 2

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -9,7 +9,7 @@
         </a>
       </li>
       <!-- End Dashboard Nav -->
-      @if (Auth::user()->role == 'Admin' || Auth::user()->role == 'Guru')
+      @if (Auth::user()->role == 'Admin')
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-people-fill"></i><span>Manajemen Siswa</span><i class="bi bi-chevron-down ms-auto"></i>
@@ -27,7 +27,56 @@
           </li>
         </ul>
       </li>
-      
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-people-fill"></i><span>Inventaris</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Kelola Data User/Siswa</span>
+            </a>
+          </li>
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Cetak Kartu</span>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#components-nav3" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-file-earmark-richtext"></i><span>Manajemen Konten</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="components-nav3" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Tambah Konten</span>
+            </a>
+          </li>
+        </ul>
+      </li>
+      {{-- role guru --}}
+      @elseif(Auth::user()->role == 'Guru')
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-people-fill"></i><span>Manajemen Siswa</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Kelola Data User/Siswa</span>
+            </a>
+          </li>
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Cetak Kartu</span>
+            </a>
+          </li>
+        </ul>
+      </li>
 
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-nav3" data-bs-toggle="collapse" href="#">
@@ -59,14 +108,13 @@
       @endif
 
 
-      @if (Auth::user()->role == 'Siswa')
       <li class="nav-item">
         <a class="nav-link collapsed" href="#">
           <i class="bi bi-person-vcard"></i>
           <span>PKL/Magang</span>
         </a>
       </li>
-      @endif
+
 
       <!-- End Blank Page Nav -->
 
